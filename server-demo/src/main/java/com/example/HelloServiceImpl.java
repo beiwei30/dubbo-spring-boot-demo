ec2a68b18bec7da94e219dde9adf53165f063a31
beiwei30 <ian.luo@gmail.com>
2018-08-28 16:24:47 +0800

    reformat the code, add traceId code in service impl

--- server-demo/src/main/java/com/example/HelloServiceImpl.java
+++ server-demo/src/main/java/com/example/HelloServiceImpl.java
@@ -1,15 +1,24 @@
 package com.example;
 
-import java.util.Date;
-
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.dubbo.rpc.RpcContext;
+
+import java.util.Date;
 
 @Service(version = "1.0.0")
 public class HelloServiceImpl implements HelloService {
 
-	@Override
+    @Override
     public String sayHello(String name) {
-        return "Hello, " + name + ", " + new Date();
-    }
+        String traceId = RpcContext.getContext().getAttachment("traceId");
 
+        System.err.println("traceId: " + traceId);
+
+        String message = "Hello, " + name + ", " + new Date();
+        if (traceId != null) {
+            message += ", traceId: " + traceId;
+        }
+
+        return message;
+    }
 }
